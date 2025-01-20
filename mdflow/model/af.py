f649e31d72b7ccd25bb9dbf233341caeb3171f15
Parsa Idehpour <ididea@sas.upenn.edu>
2025-01-20 13:45:35 -0500

    fixed diffusion pipeline

--- mdflow/model/af.py
+++ mdflow/model/af.py
@@ -111,7 +111,7 @@ class AlphaFold(nn.Module):
         self.input_embedder = InputEmbedder(
             **self.config["input_embedder"],
         )
-        self.recycling_embedder = RecyclingEmbedder(
+        self.recycling_embedder = RecyclinegEmbedder(
             **self.config["recycling_embedder"],
         )
         
@@ -246,10 +246,12 @@ class AlphaFold(nn.Module):
         else:
             m_1_prev, z_prev, x_prev = prev_outputs['m_1_prev'], prev_outputs['z_prev'], prev_outputs['x_prev']
 
+        
         x_prev = pseudo_beta_fn(
-            feats["aatype"], x_prev, None
+            feats["aatype"], feats["noised_structure"], None
         ).to(dtype=z.dtype)
 
+     
         # m_1_prev_emb: [*, N, C_m]
         # z_prev_emb: [*, N, N, C_z]
         m_1_prev_emb, z_prev_emb = self.recycling_embedder(
